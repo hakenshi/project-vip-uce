5c3fa8b61f9c911bf8e610872532a6c4f20e99ab
hakenshi <fkdias025@gmail.com>
2024-09-01 12:56:39 -0300

    modified:   src/app/(admin)/alunos/DataTable.tsx

---
+++ src/app/(admin)/alunos/DataTable.tsx
@@ -0,0 +1,89 @@
+'use client'
+
+import {
+    Column,
+    getCoreRowModel,
+    getFilteredRowModel,
+    getPaginationRowModel,
+    getSortedRowModel
+} from "@tanstack/table-core";
+import {flexRender, useReactTable} from "@tanstack/react-table";
+import {Table, TableBody, TableCell, TableHead, TableHeader, TableRow} from "@/components/ui/table";
+import React from "react";
+import {Input} from "@/components/ui/input";
+import {Dialog, DialogContent, DialogDescription, DialogTitle, DialogTrigger} from "@/components/ui/dialog";
+import AvatarPreview from "@/components/AvatarPreview";
+import {Label} from "@/components/ui/label";
+import {Button} from "@/components/ui/button";
+import CadastrarAluno from "@/components/CadastrarAluno";
+
+interface DataTableProps<TData, TValue> {
+    columns: Column<TData, TValue>[];
+    data: TData[];
+}
+
+export function DataTable<TData, TValue> ({columns, data}: DataTableProps<TData, TValue>) {
+
+    const table = useReactTable({
+        data,
+        columns,
+        getCoreRowModel: getCoreRowModel(),
+        getPaginationRowModel: getPaginationRowModel(),
+        getFilteredRowModel: getFilteredRowModel(),
+        getSortedRowModel: getSortedRowModel(),
+    })
+
+    return (
+        <div className="container mx-auto py-2 border rounded overflow-x-auto">
+            <div className="flex justify-evenly p-5 items-center">
+                <div>
+                    <Input
+                        placeholder={"Insira um nome..."}
+                        value={(table.getColumn('name')?.getFilterValue() as string) ?? ''}
+                        onChange={(e) => table.getColumn('name')?.setFilterValue(e.target.value)}
+                    />
+                </div>
+                <div>
+                <CadastrarAluno />
+                </div>
+            </div>
+            <Table>
+                <TableHeader>
+                    {table.getHeaderGroups().map((headerGroup) => (
+                        <TableRow key={headerGroup.id} className={"text-center hover:bg-transparent"}>
+                            {headerGroup.headers.map((header) => {
+                                return(
+                                    <TableHead className={"text-center"} key={header.id}>
+                                        {header.isPlaceholder ? null : flexRender(
+                                            header.column.columnDef.header,
+                                            header.getContext()
+                                        )}
+                                    </TableHead>
+                                )
+                            })}
+                        </TableRow>
+                    ))}
+                </TableHeader>
+                <TableBody>
+                    {table.getRowModel().rows?.length ? (
+                        table.getRowModel().rows.map((row) => (
+                            <TableRow key={row.id} className={"text-center hover:bg-transparent"}>
+                                {row.getVisibleCells().map((cell) => (
+                                    <TableCell key={cell.id}>
+                                        {flexRender(cell.column.columnDef.cell, cell.getContext())}
+                                    </TableCell>
+                                ))}
+                            </TableRow>
+                        ))
+                    ) : (
+                        <TableRow className={"text-center"}>
+                            <TableCell colSpan={columns.length}>
+                                Sem resultados.
+                            </TableCell>
+                        </TableRow>
+                    )}
+                </TableBody>
+            </Table>
+        </div>
+    )
+}
