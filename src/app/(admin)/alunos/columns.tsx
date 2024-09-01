fb4edf76bb6da8db5fdaa686364e1e74b96ecc35
hakenshi <fkdias025@gmail.com>
2024-09-01 12:49:40 -0300

    modified:   src/app/(admin)/alunos/columns.tsx

---
+++ src/app/(admin)/alunos/columns.tsx
@@ -0,0 +1,150 @@
+'use client'
+
+import {ColumnDef} from "@tanstack/table-core";
+import {Users} from "@prisma/client";
+import {Avatar, AvatarFallback, AvatarImage} from "@/components/ui/avatar";
+import {Button} from "@/components/ui/button";
+import {faArrows, faArrowsUpDown, faCommentDots, faEllipsis, faSort} from "@fortawesome/free-solid-svg-icons";
+import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
+import {
+    DropdownMenu,
+    DropdownMenuContent,
+    DropdownMenuItem,
+    DropdownMenuLabel, DropdownMenuSeparator,
+    DropdownMenuTrigger
+} from "@/components/ui/dropdown-menu";
+import {Dialog, DialogContent, DialogHeader, DialogTitle, DialogTrigger} from "@/components/ui/dialog";
+import {useState} from "react";
+import {
+    AlertDialog, AlertDialogAction, AlertDialogCancel,
+    AlertDialogContent,
+    AlertDialogDescription, AlertDialogFooter,
+    AlertDialogHeader,
+    AlertDialogTitle
+} from "@/components/ui/alert-dialog";
+import EditarAluno from "@/components/EditarAluno";
+import {deleteUser} from "@/actions/user-crud";
+
+export const columns: ColumnDef<Users>[] = [
+    {
+        accessorKey: 'image',
+        header: 'Foto',
+        cell: ({row}) => {
+
+            const user = row.original
+
+            return (
+                   <div className={"w-full flex justify-center"}>
+                       <Avatar>
+                           <AvatarImage className={"object-cover"} src={`${user.image}`} />
+                           <AvatarFallback>
+                               {user?.name.substring(0, 2).toUpperCase()}
+                           </AvatarFallback>
+                       </Avatar>
+                   </div>
+            )
+        }
+    },
+    {
+        accessorKey: 'name',
+        header: 'Nome',
+    },
+    {
+        accessorKey: 'email',
+        header: 'Email',
+    },
+    {
+        accessorKey: 'created_at',
+        header: ({column}) => {
+            return(
+                <Button className={"space-x-2"} variant={"ghost"}
+                        onClick={() => column.toggleSorting(column.getIsSorted() === 'asc')}>
+                    <span>Data de Criação</span> <FontAwesomeIcon icon={faArrowsUpDown} />
+                </Button>
+            )
+        },
+        cell: ({row}) => {
+            const user = row.original
+            return Intl.DateTimeFormat('pt-br', {
+                dateStyle: "short",
+                timeStyle: "short",
+            }).format(user.created_at)
+        },
+        sortingFn: 'datetime'
+    },
+    {
+        id: 'actions',
+        enableHiding: false,
+        cell: ({row}) => {
+
+            const user = row.original
+
+            const [isOpen, setIsOpen] = useState(false)
+            const [isEditing, setIsEditing] = useState(false)
+            const [isAlertOpen, setIsAlertOpen] = useState(false)
+
+            return(
+                <>
+
+                    <Dialog open={isOpen} onOpenChange={() => setIsOpen(false)}>
+                        <DialogContent>
+                            Ver Aluno
+                        </DialogContent>
+                    </Dialog>
+
+                    <Dialog open={isEditing} onOpenChange={() => setIsEditing(false)}>
+                        <DialogContent>
+                        <DialogHeader>
+                            <DialogTitle>Editar Aluno</DialogTitle>
+                        </DialogHeader>
+                            <EditarAluno user={user} />
+                        </DialogContent>
+                    </Dialog>
+                    <AlertDialog open={isAlertOpen} onOpenChange={() => setIsAlertOpen(false)} >
+                        <AlertDialogContent>
+                            <AlertDialogHeader>
+                                <AlertDialogTitle>Tem certeza de que deseja excluir este aluno?</AlertDialogTitle>
+                            </AlertDialogHeader>
+                            <AlertDialogDescription>
+                                Essa ação é irreversível
+                            </AlertDialogDescription>
+                            <AlertDialogFooter>
+                                <AlertDialogAction onClick={async () => {
+                                   const response = await deleteUser(user.email)
+                                    console.log(response)
+                                }} >Sim</AlertDialogAction>
+                                <AlertDialogCancel>Não</AlertDialogCancel>
+                            </AlertDialogFooter>
+                        </AlertDialogContent>
+                    </AlertDialog>
+                    <DropdownMenu>
+                        <DropdownMenuTrigger asChild>
+                            <button>
+                                <span className={"sr-only"}>Opções</span>
+                                <FontAwesomeIcon icon={faEllipsis} />
+                            </button>
+                        </DropdownMenuTrigger>
+                        <DropdownMenuContent>
+                            <DropdownMenuLabel>Ações</DropdownMenuLabel>
+                            <DropdownMenuSeparator />
+                            <DropdownMenuItem onClick={() =>setIsOpen(true)}>
+                                Ver Aluno
+                            </DropdownMenuItem>
+                            <DropdownMenuItem onClick={() => setIsEditing(true)}>
+                                Editar Aluno
+                            </DropdownMenuItem>
+                            <DropdownMenuItem onClick={() => setIsAlertOpen(true)}>
+                                Excluir Aluno
+                            </DropdownMenuItem>
+                            <DropdownMenuSeparator />
+                            <DropdownMenuItem onClick={()=> navigator.clipboard.writeText(user.name)}>
+                                Copiar Nome
+                            </DropdownMenuItem>
+                        </DropdownMenuContent>
+
+                    </DropdownMenu>
+                </>
+            )
+        }
+    }
+]
