fb6ba3469214bd1afbc7ac8c5478b097845bbe73
hakenshi <fkdias025@gmail.com>
2024-11-12 16:25:50 -0300

    fixed notification rendering

--- src/components/Notificacao.tsx
+++ src/components/Notificacao.tsx
@@ -9,6 +9,7 @@ import {
 
 import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
 import {faBell as faBellRegular} from "@fortawesome/free-regular-svg-icons";
+import {faBell as faBellSolid} from "@fortawesome/free-solid-svg-icons";
 import {Button} from "@/components/ui/button";
 import {useEffect, useState} from "react";
 import {io} from "socket.io-client";
@@ -18,53 +19,73 @@ import {Notifications} from "@prisma/client";
 import {response} from "express";
 
 export default function Notificacao({classId}: { classId: number }) {
-    const [notificacoes, setNotificacoes] = useState<string[] | []>([])
+    const [notificacoes, setNotificacoes] = useState<Notifications[]>([]);
+
+    const fetchNotificacoes = async () => {
+        try {
+            const response = await fetch(`http://localhost:8000/turma/notificao/${classId}`);
+            const data: Notifications[] = await response.json();
+
+            setNotificacoes(data);
+
+        } catch (error) {
+            console.error("Erro ao buscar notificações:", error);
+        }
+    };
+
+    const getMessage = (message: "MESSAGE" | "ALERT") => {
+        const messageTypes = {
+            "MESSAGE": "Você tem uma nova atividade",
+            "ALERT": "Você tem uma atividade pendente.",
+        };
+        const notificationMessage = messageTypes[message];
+        if (!notificationMessage) {
+            alert("Tipo de mensagem inexistente");
+            return;
+        }
+        return {notificationType: messageTypes, notificationMessage};
+    };
 
     useEffect(() => {
-        socket.on(`turma-${classId}`, async ({notification}) => {
-            const response = await fetch(`http://localhost:8000/turma/notificao/${classId}`)
-            const data: Notifications = await response.json()
-            console.log(notification)
-        })
+        fetchNotificacoes()
 
-        // fetch(`http://localhost:8000/turma/notificao/${classId}`).then(response=>{
-        //     if(response.ok){
-        //         return response.json()
-        //     }
-        // }).then(data=>{
-        //     console.log(data)
-        // })
-
-    }, [notificacoes])
-
-    console.log(notificacoes)
+        socket.on(`turma-${classId}`, ({notification}) => {
+            fetchNotificacoes(); // Busca notificações novamente quando houver uma atualização
+        });
+    }, [classId]);
 
     const limparNotificacao = () => {
-        setNotificacoes([])
-    }
+        setNotificacoes([]);
+    };
 
     return (
         <>
             <DropdownMenu>
                 <DropdownMenuTrigger>
-                    <FontAwesomeIcon className={"text-xl text-white"} icon={faBellRegular}/>
+                    <FontAwesomeIcon className="text-xl text-white"
+                                     icon={notificacoes.length > 0 ? faBellSolid : faBellRegular}/>
                 </DropdownMenuTrigger>
                 <DropdownMenuContent>
-                    <DropdownMenuLabel>Notificações</DropdownMenuLabel>
+                    <DropdownMenuLabel>Notificações ({notificacoes.length})</DropdownMenuLabel>
                     <DropdownMenuSeparator/>
-                    {notificacoes.length === 0 ?
-                        <DropdownMenuItem>Nada para exibir</DropdownMenuItem> :
+                    {notificacoes.length === 0 ? (
+                        <DropdownMenuItem>Nada para exibir</DropdownMenuItem>
+                    ) : (
                         notificacoes.map((notificacao, i) => (
-                            <DropdownMenuItem key={i}><Link href={"/atividades"}>{notificacao}</Link></DropdownMenuItem>
-                        ))}
+                            <DropdownMenuItem key={i}>
+                                <Link
+                                    href="/atividades">{notificacao ? getMessage(notificacao.notificationType)?.notificationMessage : ""}</Link>
+                            </DropdownMenuItem>
+                        ))
+                    )}
                     <DropdownMenuSeparator/>
-                    <DropdownMenuItem className={"p-0 m-0"}>
-                        <Button onClick={() => limparNotificacao()} className={"w-full p-0 m-0"}>
+                    <DropdownMenuItem className="p-0 m-0">
+                        <Button onClick={limparNotificacao} className="w-full p-0 m-0">
                             Limpar
                         </Button>
                     </DropdownMenuItem>
                 </DropdownMenuContent>
             </DropdownMenu>
         </>
-    )
+    );
 }
